c5df8f094089143cd92adf2aa72373a5eac5c299
Tom Roth <tom.roth.2003@gmail.com>
2024-12-08 15:18:43 +0100

    refactor raycast

--- actiongame/scenes/camera/camera.gd
+++ actiongame/scenes/camera/camera.gd
@@ -12,12 +12,18 @@ func _process(delta: float) -> void:
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		var from = self.project_ray_origin(event.position)
-		var to = from + self.project_ray_normal(event.position) * RAY_LENGTH
-		var space_state = get_world_3d().direct_space_state
-		var query = PhysicsRayQueryParameters3D.create(from, to)
-		var result = space_state.intersect_ray(query)
+		var result = raycast_mouse(event.position)
 		var player = get_tree().get_nodes_in_group("Player")[0]
 		if "position" in result:
 			player._on_move_towards(result.position)
 		
+	if Input.is_action_pressed("player_attack"):
+		var result = raycast_mouse(event.position)
+		# TODO: attack
+
+func raycast_mouse(mouse: Vector2) -> Dictionary:
+	var from = self.project_ray_origin(mouse)
+	var to = from + self.project_ray_normal(mouse) * RAY_LENGTH
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(from, to)
+	return  space_state.intersect_ray(query)
